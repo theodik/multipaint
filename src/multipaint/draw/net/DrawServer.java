5240f97029c53eeba40ce881226242bec3de34a8
theodik <theodik@theodik-laptop>
2012-06-10 21:39:23 +0200

    add client class

--- src/multipaint/draw/net/DrawServer.java
+++ src/multipaint/draw/net/DrawServer.java
@@ -1,29 +1,44 @@
 package multipaint.draw.net;
 
-import java.awt.Color;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
-import java.nio.channels.Pipe;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import multipaint.draw.Canvas;
-import multipaint.draw.tools.Tool;
+import multipaint.draw.net.server.Client;
 
 public class DrawServer implements DrawSocket {
-    private ServerThread cthread;
+    private ServerThread sthread;
 
     @Override
     public void disconnect() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (sthread == null) {
+            return;
+        }
+        try {
+            sthread.interrupt();
+            sthread.join();
+        } catch (InterruptedException e) {
+        } finally {
+            sthread = null;
+        }
     }
 
     @Override
     public void connect(Canvas canvas, String host, int port) throws DrawNetException {
-        if (cthread != null) {
+        if (sthread != null) {
             throw new DrawNetException("Already connected.");
         }
         try {
-            cthread = new ServerThread(canvas, port);
-            cthread.start();
+            sthread = new ServerThread(canvas, port);
+            sthread.start();
         } catch (IOException e) {
             DrawNetException ex = new DrawNetException();
             ex.addSuppressed(e);
@@ -31,12 +46,22 @@ public class DrawServer implements DrawSocket {
         }
     }
 
+    public Set<Client> getClientList() {
+        if (sthread == null) {
+            throw new IllegalStateException("Server is not bound.");
+        }
+        return sthread.getClientList();
+    }
+
     private class ServerThread extends Thread {
+        private static final int BUFFER_SIZE = 512;
         private final Charset charset = Charset.forName("UTF-8");
+        HashSet<Client> clientList = new HashSet<>();
         private final Canvas canvas;
         private final InetSocketAddress address;
         private final SynchronizedCanvasListener listener;
         private final Pipe pipe;
+        private DatagramChannel channel;
 
         public ServerThread(Canvas canvas, int port) throws IOException {
             this.pipe = Pipe.open();
@@ -48,7 +73,114 @@ public class DrawServer implements DrawSocket {
 
         @Override
         public void run() {
-            
+            //-- initialize --
+            Pipe.SourceChannel source = pipe.source();
+            Selector selector;
+            try {
+                selector = Selector.open();
+
+                channel = DatagramChannel.open();
+                channel.configureBlocking(false);
+                channel.register(selector, SelectionKey.OP_READ);
+                channel.bind(address);
+
+                source.configureBlocking(false);
+                source.register(selector, SelectionKey.OP_READ);
+            } catch (IOException e) {
+                System.err.println("Error in creating server: " + e);
+                return;
+            }
+
+            //-- process --
+            ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+            main:
+            while (!interrupted()) {
+                try {
+                    selector.select();
+                } catch (IOException e) {
+                }
+
+
+                for (Iterator<SelectionKey> it = selector.selectedKeys().iterator(); it.hasNext();) {
+                    SelectionKey key = it.next();
+                    buffer.clear();
+                    try {
+                        if (key.channel() == channel) {
+                            Client client = new Client(channel.receive(buffer));
+                            if (clientList.add(client)) {
+                                client.update();
+                            }
+                            sendToAll(buffer, client);
+                        } else if (key.channel() == source) {
+                            source.read(buffer);
+                            sendToAll(buffer);
+                        }
+                    } catch (ClosedByInterruptException c) {
+                        break main;
+                    } catch (IOException e) {
+                        System.out.println("Exception in sending: " + e);
+                    }
+                }
+            }
+
+            //-- finish --
+
         }
+
+        private void sendToAll(String data) throws IOException {
+            sendToAll(charset.encode(data));
+        }
+
+        private void sendToAll(ByteBuffer data) throws IOException {
+            sendToAll(data, null);
+        }
+
+        private void sendToAll(ByteBuffer data, Client except) throws IOException {
+            for (Iterator<Client> it = clientList.iterator(); it.hasNext();) {
+                Client client = it.next();
+                if (!client.isConnected()) {
+                    it.remove();
+                }
+                if (client == except) {
+                    continue;
+                }
+                data.flip();
+                channel.send(data, client.getAddress());
+            }
+        }
+
+        private Set<Client> getClientList() {
+            return Collections.unmodifiableSet(clientList);
+        }
+    }
+
+    public static void main(String[] args) {
+        DrawServer server = new DrawServer();
+        try {
+            System.out.println("Listening at port 1234...");
+            server.connect(new Canvas(100, 100), null, 1234);
+        } catch (DrawNetException ex) {
+            System.err.println("Cannot create server: " + ex);
+            return;
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String data;
+        do {
+            try {
+                System.out.print("> ");
+                data = reader.readLine().trim();
+                switch (data) {
+                    case "list":
+                        System.out.println(server.getClientList());
+                        break;
+                }
+            } catch (IOException ex) {
+                System.err.println("Cannot read input: " + ex);
+                break;
+            }
+        } while (!data.equals("end"));
+        System.out.println("Disconnecting...");
+        server.disconnect();
+        System.out.println("Bye.");
     }
 }
---
+++ src/multipaint/draw/net/server/Client.java
@@ -0,0 +1,51 @@
+package multipaint.draw.net.server;
+
+import java.net.SocketAddress;
+
+public class Client {
+    private final SocketAddress address;
+    private long updated = -1;
+    private long limit = 60000;
+
+    public Client(SocketAddress address) {
+        this.address = address;
+        //updated = getTime();
+    }
+
+    private long getTime() {
+        return System.currentTimeMillis();
+    }
+
+    public SocketAddress getAddress() {
+        return address;
+    }
+
+    public void setLimit(long limit) {
+        this.limit = limit;
+    }
+
+    public long getLimit() {
+        return limit;
+    }
+
+    public boolean isConnected() {
+        return getTime() - updated < limit;
+    }
+
+    public void update() {
+        updated = getTime();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Client) {
+            return ((Client)obj).address.equals(address);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return address.toString();
+    }
+}
